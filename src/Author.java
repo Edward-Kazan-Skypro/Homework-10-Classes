5b6077160df027fc24c8386a2ec8550fe362a7ad
Edward-Kazan-Skypro <zvezda_torgi@mail.ru>
2022-10-24 12:13:01 +0300

    Домашнее задание 10 - Классы

--- src/Author.java
+++ src/Author.java
@@ -21,22 +21,16 @@ public class Author {
             System.out.println("Добавлен автор - " + name + " " + surname);
         }
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getSurname() {
         return surname;
     }
-
     public void setSurname(String surname) {
         this.surname = surname;
     }
-
-
 }
